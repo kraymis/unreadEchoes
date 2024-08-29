9f88c4a10ee8dee9702342d592ef7b4dbe043146
Aymen Maachi <kraymis.maachi@gmail.com>
2024-08-29 18:50:54 +0100

    1.41

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Set up the base URL for the API
-const API_BASE_URL = 'https://unreadechoes-backend.onrender.com/api';
+const API_BASE_URL = 'https://unreadechoes.onrender.com/api';
 
 // Function to submit a message
 export const submitMessage = async (name, messageData) => {
